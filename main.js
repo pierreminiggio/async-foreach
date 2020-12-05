4fabeef063b2ee6f5101429432045d4a6fd85897
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-05 14:35:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * @callback AsyncFunction
+ * 
+ * @param {*} value
+ * @param {number} index
+ * @param {Array} array
+ * 
+ * @returns {void}
+ */
+
+/**
+ * @param {Array} array 
+ * @param {AsyncFunction} asyncCallback 
+ */
+export default async function asyncForEach (array, asyncCallback) {
+    for (let index = 0; index < array.length; index++) {
+        await asyncCallback(array[index], index, array)
+    }
+}
